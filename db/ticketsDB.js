6638043108ae42b4d34b72c4be8d82d95c590405
Shakibul Islam <8080.shakib@gmail.com>
2024-12-10 11:40:35 +0600

    build: Complete ticket database

--- db/ticketsDB.js
+++ db/ticketsDB.js
@@ -1,12 +1,69 @@
 const Ticket = require("../models/ticket");
 
-class DB {
+class TicketsDB {
   tickets = [];
 
-  buyTicket = (username, price) => {
-    this.tickets.push(new Ticket(username, price));
-  };
+  /**
+   * Get all tickets from database
+   * @returns {Array.<Object>} An array of ticket objects
+   */
   getTickets = () => {
     return this.tickets;
   };
+
+  /**
+   * Creates a new ticket and adds to the database
+   * @param {String} username
+   * @param {Number} price
+   * @returns {Object} Returns purchased ticket
+   */
+  buyTicket = (username, price) => {
+    const ticket = new Ticket(username, price);
+    this.tickets.push(ticket);
+    return ticket;
+  };
+
+  /**
+   * Updates ticket based on tickedId
+   * @param {string} ticketId
+   * @param {string} username
+   * @returns {object} Return the updated ticket object
+   */
+  updateTicket = (ticketId, username) => {
+    const index = this.tickets.findIndex(
+      (ticket) => ticket.ticketId === ticketId
+    );
+    this.tickets[index].username = username;
+    this.tickets[index].updateTime = new Date();
+    return this.tickets[index];
+  };
+
+  /**
+   * Delete ticket based on ticketId
+   * @param {string} ticketId
+   * @returns {boolean} returns boolean based on task completion.
+   */
+  deleteTicket = (ticketId) => {
+    this.tickets = this.tickets.filter((ticket) => ticket.ticketId != ticketId);
+    return true;
+  };
+
+  /**
+   * Bulk buy tickets based on username
+   * @param {string} username
+   * @param {number} price
+   * @param {number} quantity
+   * @returns {Array.<object>} Returns array of new tickets
+   */
+  bulkBuyTickets = (username, price, quantity) => {
+    const ticketsArray = [];
+    for (let i = 0; i < quantity; i++) {
+      ticketsArray.push(this.buyTicket(username, price));
+    }
+    return ticketsArray;
+  };
+
+  draw = (winners) => {};
 }
+const DB = new TicketsDB();
+module.exports = DB;
